ae7e0eecb666ff412206d084e0d2d4bc943640f3
DmitriyShvanyk <soledar1984@gmail.com>
2020-02-18 17:41:14 +0200

    Commit 2

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ import PackList from "./modules/packList.js";
 export const initialCards = [
   {
     id: 1,
-    image: './images/device.svg',
+    image: 'https://dmitriyshvanyk.github.io/packs/images/device.svg',
     title: 'Mobile',
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     price: '10'
   },
   {
     id: 2,
-    image: './images/laptop.svg',
+    image: 'https://dmitriyshvanyk.github.io/packs/images/laptop.svg',
     title: 'Desktop',
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     price: '15'
   },
   {
     id: 3,
-    image: './images/monitor.svg',
+    image: 'https://dmitriyshvanyk.github.io/packs/images/monitor.svg',
     title: 'Tv',
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     price: '20'
